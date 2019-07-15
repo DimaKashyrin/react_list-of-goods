930037289be0340b62c21097a555343f337be71c
DimaKashyrin <kashyrindima@gmail.com>
2019-07-15 04:56:14 +0300

    Updates

---
+++ precache-manifest.e012d7e7647bc35654c8c2d22a0aa622.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3be9a48ea6bac8eabe544744a3af6a",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "3a51ce78e725912bdbba",
+    "url": "/react_list-of-goods/static/css/main.239c8b0c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_list-of-goods/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a51ce78e725912bdbba",
+    "url": "/react_list-of-goods/static/js/main.21eb9ff4.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
